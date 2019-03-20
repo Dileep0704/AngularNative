3d812b030144b0d0783b09c84a8398aa3ba2b4e1
dileep <dileep@metronconsulting.com>
2019-03-20 14:39:12 +0530

    show menu , but hardcoded merchant id

--- src/app/shared/menu/menu.service.ts
+++ src/app/shared/menu/menu.service.ts
@@ -1,23 +1,34 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject, Observable, of } from 'rxjs';
 import { ScannerConfig } from "../scanner/scanner.config";
+
+import AppConfig from "../app.config";
 
 @Injectable({
   providedIn: 'root'
 })
 export class MenuService {
 
+  menu;
   restaurantCatalog: BehaviorSubject<any>;
-  constructor() { 
+  constructor(private http: HttpClient) { 
     this.restaurantCatalog = new BehaviorSubject<any>({});
   }
 
-  getMenu(id: String) {
-    this.restaurantCatalog.next(ScannerConfig.dummyData);
+  getMenu(id) {
+    id = 5
+    this.http.get(AppConfig.baseUrl + `api/merchants/${id}/menu/`)
+    .subscribe((menu) => {
+      this.menu = menu
+      this.restaurantCatalog.next(menu);
+    })
+    //this.restaurantCatalog.next(ScannerConfig.dummyData);
   }
 
   getItemDetails(id: number): Observable<any> {
-    return of(ScannerConfig.dummyData.filter((item) => item.id === id)[0]);
+    //return of(ScannerConfig.dummyData.filter((item) => item.id === id)[0]);
+    return of(this.menu.filter((item) => item.itemId === id)[0]);
   }
 
 }
