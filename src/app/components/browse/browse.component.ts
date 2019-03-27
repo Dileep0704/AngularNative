9e54f52d6940ae831827b10843a1166c69016ce3
dileep <dileep@metronconsulting.com>
2019-03-27 17:04:23 +0530

    upload image template

--- src/app/components/browse/browse.component.ts
+++ src/app/components/browse/browse.component.ts
@@ -2,9 +2,9 @@ import { Component, OnInit } from "@angular/core";
 import { getRootView } from "tns-core-modules/application";
 import { RadSideDrawer } from "nativescript-ui-sidedrawer";
 import * as imagepicker from "nativescript-imagepicker";
-// import * as fs from "file-system";
-// import { knownFolders, path } from "file-system";
+import { File } from "tns-core-modules/file-system";
 import * as bgHttp from "nativescript-background-http";
+import { BehaviorSubject } from 'rxjs';
 
 
 @Component({
@@ -16,7 +16,13 @@ import * as bgHttp from "nativescript-background-http";
 export class BrowseComponent implements OnInit {
 
 	selectedImg;
+	currentFileNameBeingUploaded;
+	private event = new BehaviorSubject<any>({});
+	private session: any;
+	private url = "http://www.csm-testcenter.org/test";
+
 	constructor() {
+		this.session = bgHttp.session("image-upload");
 	}
 
 	ngOnInit(): void {
@@ -46,8 +52,8 @@ export class BrowseComponent implements OnInit {
 				if (imageAsset) {
 					this.getImageFilePath(imageAsset).then((path) => {
 						console.log(`path: ${path}`);
-						// this.uploadImage(path);
-						this.selectedImg = path;
+						this.selectedImg = path;						
+						this.uploadImage(path);
 					});
 				}
 
@@ -62,20 +68,60 @@ export class BrowseComponent implements OnInit {
 		});
 	}
 
-	// private uploadImage(path: string) {
-	// 	let file = fs.File.fromPath(path);
-	// 	this.currentFileNameBeingUploaded = file.path.substr(file.path.lastIndexOf("/") + 1);
+	private uploadImage(path: string) {
+		let file = File.fromPath(path);
+		this.currentFileNameBeingUploaded = file.path.substr(file.path.lastIndexOf("/") + 1);
 
-	// 	const request = this.createNewRequest();
-	// 	request.description = `uploading image ${file.path}`;
-	// 	request.headers["File-Name"] = this.currentFileNameBeingUploaded;
+		const request = this.createNewRequest();
+		request.description = `uploading image ${file.path}`;
+		request.headers["File-Name"] = this.currentFileNameBeingUploaded;
 
-	// 	let task = this.session.uploadFile(file.path, request);
+		let task = this.session.uploadFile(file.path, request);
 
-	// 	task.on("progress", this.onEvent.bind(this));
-	// 	task.on("error", this.onEvent.bind(this));
-	// 	task.on("responded", this.onEvent.bind(this));
-	// 	task.on("complete", this.onEvent.bind(this));
-	// }
+		task.on("progress", this.progressHandler);
+		task.on("complete", this.completeHandler);
+		// task.on("progress", this.onEvent.bind(this));
+		// task.on("error", this.onEvent.bind(this));
+		// task.on("responded", this.onEvent.bind(this));
+		// task.on("complete", this.onEvent.bind(this));
+	}
+
+	private createNewRequest() {
+		const request = {
+			url: this.url,
+			method: "POST",
+			headers: {
+				"Content-Type": "application/octet-stream"
+			},
+			description: "uploading file...",
+			androidAutoDeleteAfterUpload: false,
+			androidNotificationTitle: "NativeScript HTTP background"
+		};
+
+		return request;
+	}
+
+	private onEvent(e) {
+		this.event.next({
+			eventTitle: e.eventName + " " + e.object.description,
+			eventData: {
+				error: e.error ? e.error.toString() : e.error,
+				currentBytes: e.currentBytes,
+				totalBytes: e.totalBytes,
+				body: e.data,
+				// raw: JSON.stringify(e) // uncomment for debugging purposes
+			}
+		});
+		// console.log(JSON.stringify(e))
+	}
+
+	progressHandler(e) {
+		console.log("uploaded " + e.currentBytes + " / " + e.totalBytes);
+	}
+
+	completeHandler(e) {
+		console.log("received " + e.responseCode + " code");
+		var serverResponse = e.response;
+	}
 
 }
